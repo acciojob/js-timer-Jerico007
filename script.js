91c60a68ca7aea720a7ffb2015527b6c2d2cb892
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-09-07 14:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // write js code here if required
+let para = document.getElementById("timer");
+
+setInterval(()=>{
+	para.innerText = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()},  ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+},1000);
